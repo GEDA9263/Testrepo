1a58cf9da5e32a86cfbb99f39948547db54dfbad
GEDA9263 <92788114+GEDA9263@users.noreply.github.com>
2022-06-14 19:11:42 +0200

    Update Testskript.py

--- Testskript.py
+++ Testskript.py
@@ -13,28 +13,33 @@ from PIL import Image
 
 @st.cache
 def folder_assign():
-        annotation_folder = '/annotations/'
-        if not os.path.exists(os.path.abspath('.') + annotation_folder):
-            annotation_zip = tf.keras.utils.get_file('captions.zip',
-                                           cache_subdir=os.path.abspath('.'),
-                                           origin='http://images.cocodataset.org/annotations/annotations_trainval2014.zip',
-                                           extract=True)
-            annotation_file = os.path.dirname(annotation_zip)+'/annotations/captions_train2014.json'
-            os.remove(annotation_zip)
-        else:
-          annotation_file = os.path.abspath('.') +'/annotations/captions_train2014.json'
+    annotation_folder = '/annotations/'
+    if not os.path.exists(os.path.abspath('.') + annotation_folder):
+        annotation_zip = tf.keras.utils.get_file('captions.zip',
+                cache_subdir=os.path.abspath('.'),
+                origin='http://images.cocodataset.org/annotations/annotations_trainval2014.zip'
+                , extract=True)
+        annotation_file = os.path.dirname(annotation_zip) \
+            + '/annotations/captions_train2014.json'
+        os.remove(annotation_zip)
+    else:
+        annotation_file = os.path.abspath('.') \
+            + '/annotations/captions_train2014.json'
 
 # Download image files
-        image_folder = '/train2014/'
-        if not os.path.exists(os.path.abspath('.') + image_folder):
-                image_zip = tf.keras.utils.get_file('train2014.zip',
-                                      cache_subdir=os.path.abspath('.'),
-                                      origin='http://images.cocodataset.org/zips/train2014.zip',
-                                      extract=True)
-                PATH = os.path.dirname(image_zip) + image_folder
-                os.remove(image_zip)
-         else:
-                PATH = os.path.abspath('.') + image_folder
+
+    image_folder = '/train2014/'
+    if not os.path.exists(os.path.abspath('.') + image_folder):
+        image_zip = tf.keras.utils.get_file('train2014.zip',
+                cache_subdir=os.path.abspath('.'),
+                origin='http://images.cocodataset.org/zips/train2014.zip'
+                , extract=True)
+        PATH = os.path.dirname(image_zip) + image_folder
+        os.remove(image_zip)
+    else:
+        PATH = os.path.abspath('.') + image_folder
+
+folder_assign()
 
 
 folder_assign()
